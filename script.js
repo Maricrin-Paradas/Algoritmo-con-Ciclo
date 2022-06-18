a4166b9c7ab273821baf8aea12f075996782c536
MariParadas <maaricrin@gmail.com>
2022-06-17 23:02:43 -0400

    primera tarea

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-var nombre = 'Francisco'  //String
-var apellido = "Pugh"
-var nombreApellido = "Francisco Pugh"
+let ejecucion = true
+let numero1, numero2
+let resultado = 0
 
-var edad = 37 //Number
-var esProfesor = true //boolean
+//Se solicita un numero par el cual se va a sumar a si mismo la veces que quieran.
+
+numero1 = parseInt(prompt("Número a sumar solo pares"))
+numero2 = parseInt(prompt("Veces a sumar"))
+
+if(numero1 % 2 == 0){
+   for ( let i = 1; i <= numero2; i++) {
+      console.log(resultado += numero1)
+   }
+
+   alert(resultado)
+}
+
+
+
